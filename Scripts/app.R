e6b9b5efbdfb637b5e725e2ebade80b7336f410e
Lingxing <lingxing07@hotmail.com>
2020-04-07 22:57:52 +0800

    update reactive function for dashboard 1

--- Scripts/app.R
+++ Scripts/app.R
@@ -16,7 +16,8 @@ packages = c(
   "gridExtra",
   "htmlwidgets",
   "shinythemes",
-  "forcats"
+  "forcats",
+  "shinydashboard"
 )
 
 for (p in packages) {
@@ -82,15 +83,17 @@ dashboard3 <- tabItem(tabName = "dashboard3",
                                 title = "mmap",
                                fluidRow(
                                  column(4, plotOutput("tanny2")),
-                                 column(4,plotOutput("tanny3"))
+                                 column(4, plotOutput("tanny3"))
                                )))
 
 dashboard4 <- tabItem(tabName = "dashboard4",
-                      fluidPage(theme = shinytheme("united"),
-                                title = "mmap",
-                                fluidRow(
-                                  plotOutput("tanny4")
-                                )))
+                      fluidPage(
+                        theme = shinytheme("united"),
+                        title = "mmap",
+                        fluidRow(),
+                        fluidRow(plotOutput("tanny4")),
+                        fluidRow(uiOutput("tYear"))
+                      ))
 
 
 ### 2.1.2 Fill in dashboard elements ####
@@ -131,96 +134,11 @@ masterDF$mean_temp = temperature$mean_temp
 ### 3.1 import attribute data ###
 server <- function(input, output) {
   #----------------------------------------dashboard 4---------------------------------------
-  output$tanny4 <- renderPlot({
-    scatterPlot <- ggplot(masterDF,aes(x= mean_rain, y= mean_temp, color='#E69F00')) + 
-      geom_point() + 
-      scale_color_manual(values = c('#999999')) + 
-      theme(legend.position=c(0,1), legend.justification=c(0,1))
-
-    xdensity <- ggplot(masterDF, aes(x= mean_rain, fill='#E69F00')) + 
-      geom_density(alpha=.5) + 
-      scale_fill_manual(values = c('#E69F00')) + 
-      theme(legend.position = "none")
-    # xdensity
-    
-    ydensity <- ggplot(masterDF, aes(y=mean_temp, fill='#E69F00')) + 
-      geom_density(alpha=.5) + 
-      scale_fill_manual(values = c('#999999','#E69F00')) + 
-      theme(legend.position = "none")
-    # ydensity
-    
-    blankPlot <- ggplot()+geom_blank(aes(1,1))+
-      theme(plot.background = element_blank(), 
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(), 
-            panel.border = element_blank(),
-            panel.background = element_blank(),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            axis.text.x = element_blank(), 
-            axis.text.y = element_blank(),
-            axis.ticks = element_blank()
-      )
-    
-    grid.arrange(xdensity, blankPlot, scatterPlot, ydensity, 
-                 ncol=2, nrow=2, widths=c(4, 1.4), heights=c(1.4, 4))
-  })
-  #----------------------------------------dashboard 3---------------------------------------
-  output$tanny2 <- renderPlot({
-    temp <- ggplot(temperature, aes(x = mean_temp, y = Month, fill = stat(x))) +
-      geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
-      scale_x_continuous(expand = c(0, 0)) +
-      scale_y_discrete(expand = expansion(mult = c(0.01, 0.25))) +
-      scale_fill_viridis_c(name = "Temp. [C]", option = "B") +
-      labs(
-        title = 'Temperatures in Lincoln NE',
-        subtitle = 'Mean temperatures (Celcius) by month'
-      ) +
-      theme_ridges(font_size = 13, grid = TRUE) + 
-      theme(axis.title.y = element_blank())
-    tmp <- temperature %>% 
-      filter(Year == 2018)
-    
-    temp+xlim(min(tmp$mean_temp, na.rm=TRUE)-1,max(tmp$mean_temp, na.rm=TRUE)+1)
-  })
-  
-  output$tanny3 <- renderPlot({
-    rf <- ggplot(rainfall, aes(x = mean_rain, y = factor(Month), fill = stat(x))) +
-      geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
-      scale_x_continuous(expand = c(0, 0)) +
-      scale_y_discrete(expand = expand_scale(mult = c(0.01, 0.25))) +
-      scale_fill_viridis_c(name = "Rain Precipitation (mm)", option = "D") +
-      labs(
-        title = 'Temperatures in Lincoln NE',
-        subtitle = 'Mean Rainfall Precipiration (mm) by month'
-      ) +
-      theme_ridges(font_size = 13, grid = TRUE) + 
-      theme(axis.title.y = element_blank())
-    
-    tmp <- rainfall %>% 
-      filter(Year == 2018)
-    
-    rf+xlim(0,max(tmp$mean_rain, na.rm=TRUE)+5)
-  })
-  #----------------------------------------dashboard 2---------------------------------------
-  output$tanny1 <- renderPlot({
-    rain <- ggplot(na.omit(masterDF), aes(factor(Month), mean_rain))
-    rain + geom_violin(fill = "lightblue") +
-      geom_boxplot(width = 0.1,
-                   color = "white",
-                   alpha = 0.2)+
-      ggtitle ("Rainfall distribution by month") + 
-      xlab("Month") + 
-      ylab("Average Rainfall ( mm )")
-  })
-  #----------------------------------------dashboard 1----------------------------------------
-  
-  # render filters in UI 
-  output$sYear <- renderUI({
+  output$tYear <- renderUI({
     Year_min <- min(rainfall[, "Year"], na.rm = TRUE)
     Year_max <- max(rainfall[, "Year"], na.rm = TRUE)
     sliderInput(
-      inputId = "YearLX",
+      inputId = "YearTanny4",
       label = "Year:",
       min = Year_min,
       max = Year_max,
@@ -232,27 +150,147 @@ server <- function(input, output) {
     )
   })
   
+  tanny4 <- reactive({
+    masterDF[masterDF$Year ==  as.numeric(input$YearTanny4),]
+  })
+  
+  output$tanny4 <- renderPlot({
+    scatterPlot <-
+      ggplot(tanny4(),
+             aes(x = mean_rain, y = mean_temp, color = Month)) +
+      geom_point() +
+      theme(legend.position = "None")
+    
+    xdensity <-
+      ggplot(na.omit(masterDF), aes(x = mean_rain, fill = '#E69F00')) +
+      geom_density(alpha = .5) +
+      scale_fill_manual(values = c('#E69F00')) +
+      theme(legend.position = "none")
+    
+    ydensity <-
+      ggplot(na.omit(masterDF), aes(y = mean_temp, fill = '#E69F00')) +
+      geom_density(alpha = .5) +
+      scale_fill_manual(values = c('#999999', '#E69F00')) +
+      theme(legend.position = "none")
+    
+    blankPlot <- ggplot() + geom_blank(aes(1, 1)) +
+      theme(
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank()
+      )
+    
+    grid.arrange(
+      xdensity,
+      blankPlot,
+      scatterPlot,
+      ydensity,
+      ncol = 2,
+      nrow = 2,
+      widths = c(4, 1.4),
+      heights = c(1.4, 4)
+    )
+  })
+  #----------------------------------------dashboard 3---------------------------------------
+  output$tanny2 <- renderPlot({
+    temp <- ggplot(na.omit(masterDF), aes(x = mean_temp, y = Month, fill = stat(x))) +
+      geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
+      scale_x_continuous(expand = c(0, 0)) +
+      scale_y_discrete(expand = expansion(mult = c(0.01, 0.25))) +
+      scale_fill_viridis_c(name = "Temp. [C]", option = "B") +
+      labs(
+        title = 'Temperatures in Lincoln NE',
+        subtitle = 'Mean temperatures (Celcius) by month'
+      ) +
+      theme_ridges(font_size = 13, grid = TRUE) + 
+      theme(axis.title.y = element_blank())
+    tmp <- masterDF %>% 
+      filter(Year == 2018)
+    
+    temp+xlim(min(tmp$mean_temp, na.rm=TRUE)-1,max(tmp$mean_temp, na.rm=TRUE)+1)
+  })
+  
+  output$tanny3 <- renderPlot({
+    rf <- ggplot(na.omit(masterDF), aes(x = mean_rain, y = factor(Month), fill = stat(x))) +
+      geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
+      scale_x_continuous(expand = c(0, 0)) +
+      scale_y_discrete(expand = expand_scale(mult = c(0.01, 0.25))) +
+      scale_fill_viridis_c(name = "Rain Precipitation (mm)", option = "D") +
+      labs(
+        title = 'Temperatures in Lincoln NE',
+        subtitle = 'Mean Rainfall Precipiration (mm) by month'
+      ) +
+      theme_ridges(font_size = 13, grid = TRUE) + 
+      theme(axis.title.y = element_blank())
+    
+    tmp <- masterDF %>% 
+      filter(Year == 2018)
+    
+    rf+xlim(0,max(tmp$mean_rain, na.rm=TRUE)+5)
+  })
+  #----------------------------------------dashboard 2---------------------------------------
+  output$tanny1 <- renderPlot({
+    rain <- ggplot(na.omit(masterDF), aes(factor(Month), mean_rain, fill =factor(Month)))
+    rain + geom_violin(fill = "lightblue") +
+      geom_boxplot(width = 0.1,
+                   color = "white",
+                   alpha = 0.2)+
+      ggtitle ("Rainfall distribution by month") + 
+      theme(legend.position="none") +
+      xlab("Month") + 
+      ylab("Average Rainfall ( mm )")
+  })
+  
+  #----------------------------------------dashboard 1----------------------------------------
+  # render filters in UI 
+  output$sYear <- renderUI({
+    Year_min <- min(rainfall[, "Year"], na.rm = TRUE)
+    Year_max <- max(rainfall[, "Year"], na.rm = TRUE)
+    sliderInput(
+      inputId = "YearLX",
+      label = "Year:",
+      min = 2009,
+      max = Year_max,
+      value = c(2009),
+      step = 1,
+      sep = "",
+      animate = animationOptions(interval = 5000,
+                                 loop = FALSE)
+    )
+  })
+  
   output$sMonth <- renderUI({
-    Month_choices <- unique(masterDF$Month)
     selectInput(
       inputId = "MonthLX",
       label = "Month:",
-      choices = Month_choices,
+      choices = c("Jan","Feb","Mar",
+                  "Apr","May","Jun",
+                  "Jul","Aug","Sep",
+                  "Oct","Nov","Dec"),
       selected = "Jan"
     )
   })
   
-  ## define default variable for rain map
-  tmp <- masterDF %>%
-    filter(Year == 2019) %>%
-    filter(Month == 'Jan')
-  tmp <- left_join(mpsz, tmp,
-                   by = c("SUBZONE_N" = "SZ")) %>%
-         st_transform(4326)
+  tmp<-reactive({
+    tmp2 <- masterDF %>%
+      filter(Year == as.numeric(input$YearLX)) %>%
+    filter(Month == as.character(input$MonthLX))
+    
+    tmp2 <- left_join(mpsz, tmp2,
+                      by = c("SUBZONE_N" = "SZ")) %>%
+      st_transform(4326)
+  })
   
   # show rain map
   output$lxmap = renderLeaflet({
-    tm <- tm_shape(tmp)+
+    tm <- tm_shape(tmp())+
       tm_fill("mean_rain",
               style = "quantile",
               palette = "Blues") +
@@ -262,52 +300,12 @@ server <- function(input, output) {
   
   # show temp map
   output$lxmap2 = renderLeaflet({
-    tm <- tm_shape(tmp)+
+    tm <- tm_shape(tmp())+
       tm_fill("mean_temp",
               style = "quantile",
               palette = "Oranges") +
       tm_borders(alpha = 0.5)
     tmap_leaflet(tm)
-  })
-  
-  #reactive event
-  observeEvent(c(input$YearLX,input$MonthLX), {
-    
-    YEAR = as.numeric(input$YearLX)
-    MONTH = as.character(input$MonthLX)
-
-    ## supbset the data based on the choice
-    if(YEAR != 2019 || MONTH != 'Jan'){
-       tmp_new<- masterDF %>%
-         filter(Year == YEAR) %>%
-         filter(Month == MONTH)
-        
-       tmp_new <- left_join(mpsz, tmp_new,
-                            by = c("SUBZONE_N" = "SZ")) %>%
-                  st_transform(4326)
-      
-    }else{
-      tmp_new <- tmp
-    }
-    
-    # plot the subsetted ata
-    output$lxmap = renderLeaflet({
-      tm <- tm_shape(tmp_new)+
-        tm_fill("mean_rain",
-                style = "quantile",
-                palette = "Blues") +
-        tm_borders(alpha = 0.5)
-      tmap_leaflet(tm)
-    })
-    
-    output$lxmap2 = renderLeaflet({
-      tm <- tm_shape(tmp_new)+
-        tm_fill("mean_temp",
-                style = "quantile",
-                palette = "Oranges") +
-        tm_borders(alpha = 0.5)
-      tmap_leaflet(tm)
-    })
   })
 }
 ######################### 4. Finish app ##########################
